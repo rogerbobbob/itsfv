a89e368afb56ba4e798976c4d9ef4e894fbc355d
mcored <mcored@e74ef8a8-80c3-11dd-88bd-cff828a63736>
2008-09-15 23:45:53 +0000

    5.60.20.0 Added new command line flag "synchroclean" which is the same as "add_folder remove_dead"

--- iTSfv/My Project/AssemblyInfo.vb
+++ iTSfv/My Project/AssemblyInfo.vb
@@ -32,4 +32,4 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("5.0.0.0")> 
-<Assembly: AssemblyFileVersion("5.60.18.0")> 
+<Assembly: AssemblyFileVersion("5.60.20.0")> 
