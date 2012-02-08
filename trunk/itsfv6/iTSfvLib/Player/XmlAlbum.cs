aa52215c5ef6a07c61516d2b427cf5e80d3486d8
mcored <mcored@e74ef8a8-80c3-11dd-88bd-cff828a63736>
2012-02-08 07:12:56 +0000

    AddAlbum and RemoveAlbum methods to XmlAlbum

--- trunk/itsfv6/iTSfvLib/Player/XmlAlbum.cs
+++ trunk/itsfv6/iTSfvLib/Player/XmlAlbum.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace iTSfvLib.Player
+namespace iTSfvLib
 {
     /// <summary>
     /// Holds one or more discs of an album
     /// </summary>
-    class XmlAlbum
+    public class XmlAlbum
     {
+        /// <summary>
+        /// Unique ID Artist_Album_NumberOfTracks
+        /// </summary>
+        public string ID { get; set; }
     }
 }
